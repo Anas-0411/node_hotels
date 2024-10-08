43c8e79f6ee3ac5eb54624318fe9a5957af6acd7
Anas Siddique <mohammadanas0411@gmail .com>
2024-10-08 21:15:47 +0530

    shift mongodb server online

--- database.js
+++ database.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
 
 // Define the MongoDB connection URL
 // const mongoURL = process.env.MONGODB_URL_LOCAL // Replace 'mydatabase' with your database name
