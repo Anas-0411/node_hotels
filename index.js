3f65343e6a93c970a1c6bae96322016948db1d86
Anas Siddique <mohammadanas0411@gmail .com>
2024-10-10 11:49:45 +0530

    Authentication of website

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const app = express();
-const mongoose = require("./database");
+const db = require("./database");
 const bodyParser = require("body-parser");
 require("dotenv").config();
+const passport = require("./auth");
 
 app.use(express.json());
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
+
+// Middelware Function
+const logRequest = (req, res, next) => {
+  console.log(
+    `[${new Date().toLocaleString()}] Request made to :${req.originalUrl}`
+  );
+  next(); //move to next phase
+};
+// use of middleware
+app.use(logRequest);
+
+app.use(passport.initialize());
+const localAuthMiddleware = passport.authenticate("local", { session: false });
 
 app.get("/", function (req, res) {
   return res.send("Welcome to Our Hotel!");
@@ -16,9 +30,9 @@ app.get("/", function (req, res) {
 const personRoutes = require("./routes/personRoutes");
 const menuRoutes = require("./routes/menuRoutes");
 // Use the routers
-app.use("/person", personRoutes);
+app.use("/person", localAuthMiddleware, personRoutes);
 app.use("/menu", menuRoutes);
 
 app.listen(PORT, () => {
-  console.log("listening on port 3000");
+  console.log(`listening on port ${PORT}`);
 });
